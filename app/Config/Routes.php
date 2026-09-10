4aec919683954526ee27707df349531c82ccb89e
vvvtad <vettatad@gmail.com>
2026-09-10 16:19:21 +0800

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Pages::index');
+$routes->get('about', 'Pages::about');
+$routes->get('customers', 'Customers::index');
+$routes->get('users', 'Users::index');
